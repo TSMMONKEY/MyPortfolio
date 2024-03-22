6b22b6f86d46e5de1e619611bf20124c2b056887
TSMMONKEY <thabanimat24@gmail.com>
2024-03-22 09:40:30 +0200

    added the header and footer, added all the project details to the database

---
+++ include/footer.php
@@ -0,0 +1,89 @@
+
+<footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-12 me-auto mb-4">
+            <h4 class="text-white mb-3">
+              <a href="/">Thabani</a> Steve
+            </h4>
+            <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">
+              Copyright © 2024 <strong>Thabani Steve</strong>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-8">
+            <h5 class="text-white mb-3">Sitemap</h5>
+
+            <ul class="footer-menu">
+              <li class="footer-menu-item">
+                <a href="about.html" class="footer-menu-link">Projects</a>
+              </li>
+
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Contact</a>
+              </li>
+
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-lg-5 col-12 ms-auto">
+            <div class="row">
+              <div class="col-6 border-end contact-info">
+                <h6 class="mb-3" style="color: #fff;">Email:</h6>
+
+                <a href="mailto:thabanimat3@gmail.com" class="custom-link" style="text-transform:lowercase; font-size: 15px;">
+                  thabanimat3@gmail.com
+                  <i class="bi-arrow-right ms-2"></i>
+                </a>
+              </div>
+
+              <div class="col-6 contact-info">
+                <h6 class="mb-3" style="color: #fff;">Phone:</h6>
+
+                <a href="mailto:studio@company.com" class="custom-link">
+                  studio@company.com
+                  <i class="bi-arrow-right ms-2"></i>
+                </a>
+              </div>
+
+              <div class="col-6 border-top border-end contact-info">
+                <h6 class="mb-3" style="color: #fff;">My Location:</h6>
+
+                <p class="text-muted">Ellis road Bellevue, Johannesburg 2198, South Africa </p>
+              </div>
+
+              <div class="col-6 border-top contact-info">
+                <h6 class="mb-3" style="color: #fff;">My Socials:</h6>
+
+                <ul class="social-icon">
+                  <li>
+                    <a href="#" class="social-icon-link bi-messenger"></a>
+                  </li>
+
+                  <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+
+                  <li>
+                    <a href="#" class="social-icon-link bi-instagram"></a>
+                  </li>
+
+                  <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/Headroom.js"></script>
+    <script src="js/jQuery.headroom.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
